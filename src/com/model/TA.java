7f8076c8201dc65e63997d851387c1b8a92725e9
liyi.com <1513161883@qq.com>
2019-07-01 21:50:14 +0800

    更新

--- src/com/model/TA.java
+++ src/com/model/TA.java
@@ -5,5 +5,54 @@ package com.model;
  */
 
 public class TA extends Graduate {
+    // 教师编号
+    private int teacherNo;
+    // 学校
+    private String schoolName;
+    // 部门
+    private String department;
 
+    // getter setter
+
+    public int getTeacherNo() {
+        return teacherNo;
+    }
+
+    public void setTeacherNo(int teacherNo) {
+        this.teacherNo = teacherNo;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "TA{" +
+                "user_name=" + getUser_name() +
+                "id_=" + getId_() + '\'' +
+                "gender=" + getGender() + '\'' +
+                "birthday=" + getBirthday() + '\'' +
+                "studentNo=" + getStudentNo() + '\'' +
+                "school='" + getSchool() + '\'' +
+                "class_='" + getClass_() + '\'' +
+                "direction'" + getDirection() + '\'' +
+                "adviserName'" + getAdviserName() + '\'' +
+                "teacherNo=" + teacherNo + '\'' +
+                "schoolName='" + schoolName + '\'' +
+                "department='" + department + '\'' +
+                '}';
+    }
 }
